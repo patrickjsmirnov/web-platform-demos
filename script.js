8b9696916d1fce11bd052b640dfddffffa45811f
Dmitry Smirnov <patrickjsmirnov@gmail.com>
2024-07-22 20:09:23 +0200

    dialog

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("test");
+"use strict";
+
+const dialogButton = document.querySelector(".dialog-button");
+const dialog = document.querySelector(".dialog");
+const dialogCloseButton = document.querySelector(".dialog-close-button");
+
+dialogButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+dialogCloseButton.addEventListener("click", () => {
+  dialog.close();
+});
